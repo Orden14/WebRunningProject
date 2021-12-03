780f12900abeac6b9933a47aa370cd21bdda3468
Orden14 <92043415+Orden14@users.noreply.github.com>
2021-12-03 09:35:24 +0100

    Delete ressources directory

--- ressources/main.js
+++
@@ -1,8 +0,0 @@
-function topBar() {
-  var x = document.getElementById("myTopbar");
-  if (x.className === "topbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "topbar";
-  }
-}
